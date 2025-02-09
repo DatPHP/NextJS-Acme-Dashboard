85c8ea4df3f1916e34ab4ff53f0e202a0c225cb6
DatPHP <nguyenvandat170296@gmail.com>
2025-02-09 09:24:43 +0700

    focus

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,7 +17,6 @@ export default async function Page() {
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,28 @@
+
+import QueryProvider from "./components/QueryProvider";
+// import { Roboto } from "next/font/google";
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+// const roboto = Roboto({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "700"], // Optional: Customize weights
+// });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      // className={roboto.className}
+      suppressHydrationWarning={true}
+    >
+      <body className={`${inter.className} antialiased`}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
